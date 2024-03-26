fcb3bf8532066cde89f0b63a94ea38f5402c46cf
dgsw28 <dgsw28@dgsw28ui-MacBookAir.local>
2024-03-26 19:22:29 +0900

    코드업 2

---
+++ 코드업.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a // 3 == 1:
+    print('spring')
+else:
+    if a // 3 == 2:
+        print('summer')
+    else:
+        if a // 3 == 3:
+            print('fall')
+        else:
+            print('winter')
